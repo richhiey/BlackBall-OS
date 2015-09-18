164b4bff58cabf1f4d265ec4d523b45f99a789f6
Richhiey Thomas <richhiey.thomas@gmail.com>
2015-09-18 08:37:54 +0530

    M

--- kernel/include/process/IncRoundRobin.h
+++ kernel/include/process/IncRoundRobin.h
@@ -4,16 +4,13 @@
 #ifndef ROUND_ROBIN_H
 #define ROUND_ROBIN_H
 
-#include <Process/ProcessManager.h>
-
+#include <Process/IncProcessManager.h>
 
 void RoundRobin_addProcess(Process* process);		//Add Process
-
 
 void RoundRobin_removeProcess(Process* process);	//Remove process
 
 Process* RoundRobin_getNextProcess(void);			//Get Next process
-
 
 Process* RoundRobin_getCurrentProcess(void);		//Get current process
 
