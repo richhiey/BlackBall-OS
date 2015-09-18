4606ba777d61b9eb742d56512a431894706e1056
Richhiey Thomas <richhiey.thomas@gmail.com>
2015-09-18 08:43:26 +0530

    M

--- kernel/include/process/IncProcessManager.h
+++ kernel/include/process/IncProcessManager.h
@@ -4,23 +4,23 @@
 #define PROCESS_M_H
 
 #include <module.h>
-#include <X86/IDT.h>
-#include <FileSystem/VFS.h>
-#include <Utils/ArrayList.h>
+//#include <X86/IDT.h>				TODO
+//#include <FileSystem/VFS.h>		TODO
+//#include <Utils/ArrayList.h>		TODO
 
 #define KERNEL_PID 0   	//Process ID	
 
 #define KERNEL_PROCESS 0		//Process tyoe
 #define USER_PROCESS   1
 
+//Process states
 #define PROCESS_CREATED     1
 #define PROCESS_WAITING     2
 #define PROCESS_RUNNING     3
 #define PROCESS_BLOCKED     4
 #define PROCESS_TERMINATED  5
 
-typedef struct Process Process;
-
+//Structure containing information which makes up a process
 struct Process
 {
     u32int pid;
@@ -37,11 +37,14 @@ struct Process
     ArrayList* fileNodes;
 };
 
+typedef struct Process Process;
+
+//Important functions for process management
 void  ProcessManager_switch(Regs* context); 		//Context switch method
 
 void ProcessManager_killProcess(int exitCode);		//End current process and move on to next
 
-Process* ProcessManager_spawnProcess(const char* binary);
+Process* ProcessManager_spawnProcess(const char* binary);	//Spawns a child process
 
 coid ProcessManager_waitPID(Process* process);  	//Wait till current finishes
 
