9464aa176adba9522528d4d1d58b2cd7c1b3897b
Richhiey Thomas <richhiey.thomas@gmail.com>
2015-09-18 08:48:47 +0530

    M

--- kernel/include/process/IncScheduler.h
+++ kernel/include/process/IncScheduler.h
@@ -1,24 +1,18 @@
 //Schduler, bryceleen ka hai
-
+//Scheduler which can be made to use a certain scheduing algo (In this case Round Robin)
 
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
 
-#include <Process/ProcessManager.h>
-
-
-
+#include <Process/IncProcessManager.h>
 
 extern void (*Scheduler_addProcess) (Process* process);		//Add process to scheduler
-
 
 extern void (*Scheduler_removeProcess) (Process* process);		//Remove
 
 extern Process* (*Scheduler_getNextProcess) (void);				//fetch next
 
-
 extern Process* (*Scheduler_getCurrentProcess) (void);			//Return current
-
 
 void Scheduler_init(void);										//initializing schudlar
 
